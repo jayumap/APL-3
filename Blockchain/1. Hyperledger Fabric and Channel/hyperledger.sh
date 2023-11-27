6091daf1f8bd65609e4188b03e8bd7d5dab7295c
jayumap <umapjay4757@gmail.com>
2023-11-27 05:46:53 +0530

    bct n cc updated

---
+++ Blockchain/1. Hyperledger Fabric and Channel/hyperledger.sh
@@ -0,0 +1,27 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker–compose --version
+
+sudo chmod 777 /var/run/docker.sock
+
+curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.13 1.5.6
